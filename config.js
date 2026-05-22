d802d83431460d9f3572d237c6878b19c3cbae64
sergiygav-byte <sergiy.gav@gmail.com>
2026-05-22 23:13:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 // Supabase → Project Settings → API
 window.SUPABASE_URL = 'https://lsoieljknndrtsyeolbd.supabase.co';
 window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxzb2llbGprbm5kcnRzeWVvbGJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0NjQxNjMsImV4cCI6MjA5NTA0MDE2M30.5v3QARkAzFSnkEcfIaos9h5YxB9doDO6sRtRIqdtJ_E';
+window.NOTIFY_API_URL = 'https://pnu-dorm-miniapp.vercel.app/api/notify';
+window.NOTIFY_SECRET = 'admin1177';
